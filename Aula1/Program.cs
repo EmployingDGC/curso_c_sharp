a6956a71ff9f6ef4c98b73647dc20d9f9ddea434
EmployingDGC <63275906+EmployingDGC@users.noreply.github.com>
2020-06-20 18:49:59 -0300

    Update Aula1

--- Aula1/Program.cs
+++ Aula1/Program.cs
@@ -16,14 +16,14 @@ namespace Aula1
             ushort  x6  = 52470;             // |    2  | 0 até 65535
             uint    x7  = 3295962579;        // |    4  | 0 até 2 ^ 32 - 1
             ulong   x8  = 5651898772159478L; // |    8  | 0 até 2 ^ 64 - 1
-            float   x9  = 1.154872123F;      // |    4  | ±1.5 * 10 ^ -45 até ±3.4 * 10 ^ 38 com 7 números significativos
+            float   x9  = 1.755F;            // |    4  | ±1.5 * 10 ^ -45 até ±3.4 * 10 ^ 38 com 7 números significativos
             double  x10 = 1.521323132352;    // |    8  | ±5.0 * 10 ^ -324 até ±1.7 * 10 ^ 308 com 15 ou 16 números significativos
             decimal x11 = 1.1231M;           // |   12  | ±1.0 * 10 ^ -28 até ±7.9 * 10 ^ 28 com 28 ou 29 números significativos
             char    x12 = '\u0041';          // |    2  | Qualquer caractere Unicode
             bool    x13 = true;              // |  1/2  | true ou false
 
             string  x14 = "Davi GC";         // |       | Conjunto de caracteres
-            object  x15 = 10;                // |       | Variável genérica
+            object  x15 = 23;                // |       | Variável genérica
 
             // Nomes de variáveis
 
@@ -49,8 +49,12 @@ namespace Aula1
             Console.WriteLine(x14);
             Console.WriteLine(x15);
             
-            Console.WriteLine(sbyte.MinValue); // Serve pra todos os valores
+            Console.WriteLine(sbyte.MinValue);   // Serve pra todos os valores
             Console.WriteLine(decimal.MaxValue); // Serve pra todos os valores
+
+            // Placeholders
+
+            Console.WriteLine($"Nome: {x14}\nIdade: {x15}\nAltura: {x9:F2}");
         }
     }
 }
