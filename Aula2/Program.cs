ab355e60d1539bb7373d824e621eba1621c00a67
EmployingDGC <63275906+EmployingDGC@users.noreply.github.com>
2020-06-21 11:02:22 -0300

    Update Aula2

--- Aula2/Program.cs
+++ Aula2/Program.cs
@@ -6,7 +6,24 @@ namespace Aula2
     {
         static void Main(string[] args)
         {
+            string frase = Console.ReadLine();
+
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
             
+            string[] aux = Console.ReadLine().Split(' ');
+            string a = aux[0];
+            string b = aux[1];
+            string c = aux[2];
+
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
         }
     }
 }
